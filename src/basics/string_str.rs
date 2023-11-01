ed29ac4af3bb2f715c314fb5117c547baacff0f8
DerekDuan <duan.shiqi@gmail.com>
2023-11-01 10:50:30 +0800

    Add

---
+++ src/basics/string_str.rs
@@ -0,0 +1,18 @@
+// 字符串 和 字符串 slice
+
+// String 类型
+// 索引结构：prr + len + capacity
+
+// str 类型
+// 索引结构：ptr + len
+
+// 互相转换
+
+// 相关操作
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test() {}
+}
